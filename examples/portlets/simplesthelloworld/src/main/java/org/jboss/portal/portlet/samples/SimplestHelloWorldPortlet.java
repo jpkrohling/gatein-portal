60cdf6a91e02a0a5b2093e5907761afb1d2339f0
Juraci Paixão Kröhling <juraci@kroehling.de>
2014-05-19 10:47:44 +0200

    Changed the simplest portlet to have preferences

--- examples/portlets/simplesthelloworld/src/main/java/org/jboss/portal/portlet/samples/SimplestHelloWorldPortlet.java
+++ examples/portlets/simplesthelloworld/src/main/java/org/jboss/portal/portlet/samples/SimplestHelloWorldPortlet.java
@@ -28,12 +28,51 @@ import java.io.PrintWriter;
 import javax.portlet.GenericPortlet;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
+import javax.portlet.PortletException;
+import javax.portlet.UnavailableException;
+import javax.portlet.ActionRequest;
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletSecurityException;
+import javax.portlet.ActionResponse;
 
 
 public class SimplestHelloWorldPortlet extends GenericPortlet {
-    public void doView(RenderRequest request, RenderResponse response) throws IOException {
+    protected void doView(RenderRequest request, RenderResponse response) throws PortletException, IOException
+    {
+        response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
-        writer.write("Hello World !");
+        PortletPreferences prefs = request.getPreferences();
+        String color = prefs.getValue("color", "not-defined");
+        writer.write("<div style='color:" + color + "'>Portlet Preference value: " + color + "</div>");
         writer.close();
+    }
+
+    protected void doEdit(RenderRequest request, RenderResponse response) throws PortletException, IOException
+    {
+        response.setContentType("text/html");
+        PortletPreferences prefs = request.getPreferences();
+        String currentColor = prefs.getValue("color", "");
+        String[] possibleColors = { "black", "blue", "yellow", "green", "red", "orange" };
+        PrintWriter writer = response.getWriter();
+        writer.write("<div>Portlet Preferences</div>");
+        writer.write("<form action='" + response.createActionURL() + "' method='POST'>");
+        writer.write("Select a color: ");
+        writer.write("<select name='color'>");
+        for (String c : possibleColors) {
+            writer.write("<option" + (c.equals(currentColor) ? " selected" : "") + ">" + c + "</option>");
+        }
+        writer.write("</select>");
+        writer.write("<input type='submit' value='Save'/>");
+        writer.write("</form>");
+        writer.close();
+    }
+
+    public void processAction(ActionRequest request, ActionResponse response) throws PortletException, IOException
+    {
+        String selectedColor = request.getParameter("color");
+        PortletPreferences prefs = request.getPreferences();
+        String currentColor = prefs.getValue("color", "");
+        prefs.setValue("color", selectedColor);
+        prefs.store();
     }
 }
